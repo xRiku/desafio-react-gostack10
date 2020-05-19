8eb0dc8ee9a29c3199016b1d95653d02e655a377
Philipe A. Marques <lipe.aguiar.marques@gmail.com>
2020-05-18 21:25:43 -0300

    Post estilizado

--- src/components/Post/styles.js
+++ src/components/Post/styles.js
@@ -9,21 +9,22 @@ export const PostContainer = styled.li`
     'avatar date'
     'text text';
   grid-template-rows: 30px auto 50px;
-  grid-template-columns: 100px auto auto;
+  grid-template-columns: 70px auto auto;
   img {
     grid-area: avatar;
     border-radius: 50%;
-    padding: 10px 10px;
-    width: 100px;
+    padding: 10px 5px;
+    width: 70px;
   }
   h1 {
     grid-area: name;
     color: #4a4a4a;
     font-size: 20px;
-    background-color: red;
+    padding: 10px 0 5px 5px;
   }
   h2 {
-    background-color: blue;
+    padding: 0px 5px;
+    font-size: 15px;
     grid-area: date;
     color: #9a9a9a;
   }
