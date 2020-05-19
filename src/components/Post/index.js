624f51bdbaf6acf67b311cf560677e9acc908f9b
Philipe A. Marques <lipe.aguiar.marques@gmail.com>
2020-05-18 21:16:05 -0300

    Post em forma de grid

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -6,10 +6,10 @@ function Post(props) {
   return (
     <PostContainer>
       <img src={data.author.avatar} alt={data.author.name} />
-      <PostUser>
-        <h1>{data.author.name}</h1>
-        <h2>{data.date}</h2>
-      </PostUser>
+      {/* <PostUser> */}
+      <h1>{data.author.name}</h1>
+      <h2>{data.date}</h2>
+      {/* </PostUser> */}
       <h3>{data.content}</h3>
     </PostContainer>
   );
--- src/components/Post/styles.js
+++ src/components/Post/styles.js
@@ -1,28 +1,35 @@
 import styled from 'styled-components';
 
 export const PostContainer = styled.li`
-  display: flex;
-  align-items: center;
-  justify-content: flex-start;
+  display: grid;
+  max-width: 500px;
   background-color: #f5f5f5;
+  grid-template-areas:
+    'avatar name'
+    'avatar date'
+    'text text';
+  grid-template-rows: 30px auto 50px;
+  grid-template-columns: 100px auto auto;
   img {
+    grid-area: avatar;
     border-radius: 50%;
-    padding: 50px 10px;
+    padding: 10px 10px;
     width: 100px;
   }
-  h3 {
-    color: #3e3e3e;
-    display: block;
-  }
-`;
-
-export const PostUser = styled.div`
-  float: left;
   h1 {
+    grid-area: name;
     color: #4a4a4a;
-    font-size: 26px;
+    font-size: 20px;
+    background-color: red;
   }
   h2 {
+    background-color: blue;
+    grid-area: date;
     color: #9a9a9a;
+  }
+  h3 {
+    grid-area: text;
+    color: #3e3e3e;
+    padding: 10px;
   }
 `;
